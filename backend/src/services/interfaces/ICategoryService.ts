015564b2ccdb908b626b2504bc8d0df3580d61e9
Shahriyar117 <shahriyar.rahim333@gmail.com>
2024-07-01 19:33:46 -0400

    feature: Created the category services

--- backend/src/services/interfaces/ICategoryService.ts
+++ backend/src/services/interfaces/ICategoryService.ts
@@ -1,1 +1,10 @@
-export interface ICategoryService {}
+import { Category } from "@prisma/client";
+
+export interface ICategoryService {
+  getAllCategories(
+    page: number,
+    pageSize: number,
+    orderByField: string,
+    sortOrder: "asc" | "desc"
+  ): Promise<Category[]>;
+}
