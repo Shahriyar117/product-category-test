ab62b7b7080a73d765349983878e289a57c24886
Shahriyar117 <shahriyar.rahim333@gmail.com>
2024-07-01 19:31:53 -0400

    feature: Created the category repository and model

--- backend/src/repositories/category.respository.ts
+++ backend/src/repositories/category.respository.ts
@@ -1,5 +1,32 @@
 import { injectable } from "inversify";
 import { ICategoryRepository } from "./interfaces/ICategoryRepository";
+import { PrismaClient } from "@prisma/client";
 
 @injectable()
-export class CategoryRepository implements ICategoryRepository {}
+export class CategoryRepository implements ICategoryRepository {
+  _prisma: PrismaClient;
+
+  constructor() {
+    this._prisma = new PrismaClient();
+  }
+
+  async findAll(
+    skip: number,
+    take: number,
+    orderByField: string,
+    sortOrder: "asc" | "desc"
+  ): Promise<any> {
+    const categories = await this._prisma.category.findMany({
+      take,
+      skip,
+      orderBy: {
+        [orderByField]: sortOrder,
+      },
+      include: {
+        subcategories: true,
+        products: true,
+      },
+    });
+    return categories;
+  }
+}
--- backend/src/repositories/interfaces/ICategoryRepository.ts
+++ backend/src/repositories/interfaces/ICategoryRepository.ts
@@ -1,1 +1,8 @@
-export interface ICategoryRepository {}
+export interface ICategoryRepository {
+  findAll(
+    skip: number,
+    take: number,
+    orderByField: string,
+    sortOrder: "asc" | "desc"
+  ): Promise<any>;
+}
