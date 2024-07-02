deec96c0512199cd4d50556ad49f178b65285640
Shahriyar117 <shahriyar.rahim333@gmail.com>
2024-07-01 20:35:17 -0400

    feature: Implemented the repositories

--- backend/src/repositories/category.respository.ts
+++ backend/src/repositories/category.respository.ts
@@ -1,6 +1,7 @@
 import { injectable } from "inversify";
 import { ICategoryRepository } from "./interfaces/ICategoryRepository";
 import { PrismaClient } from "@prisma/client";
+import { Category } from "../models/category.model";
 
 @injectable()
 export class CategoryRepository implements ICategoryRepository {
@@ -8,6 +9,38 @@ export class CategoryRepository implements ICategoryRepository {
 
   constructor() {
     this._prisma = new PrismaClient();
+  }
+
+  async findOrCreateWithinTransaction(
+    prisma: PrismaClient,
+    categoryName: string,
+    parentCategoryId: number | null
+  ): Promise<Category | null> {
+    let category = await prisma.category.findFirst({
+      where: { name: categoryName },
+    });
+
+    if (parentCategoryId && category) {
+      await prisma.category.update({
+        where: {
+          name: categoryName,
+        },
+        data: {
+          parentId: parentCategoryId,
+        },
+      });
+    }
+
+    if (!category) {
+      category = await prisma.category.create({
+        data: {
+          name: categoryName,
+          parentId: parentCategoryId,
+        },
+      });
+    }
+
+    return category;
   }
 
   async findAll(
