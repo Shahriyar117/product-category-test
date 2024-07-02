7e9de5b13ae14466ff575a46821f02a60809f28e
Shahriyar117 <shahriyar.rahim333@gmail.com>
2024-07-01 20:36:39 -0400

    feature: Implemented the services

--- backend/src/services/interfaces/IProductService.ts
+++ backend/src/services/interfaces/IProductService.ts
@@ -1,6 +1,13 @@
 import { Product } from "../../models/product.model";
 
 export interface IProductService {
+  createProduct(
+    data: Omit<Product, "id | categories"> & {
+      parentCategory: string;
+      categories: string[];
+    }
+  ): Promise<Product>;
+
   getAllProducts(
     page: number,
     pageSize: number,
