b8dc2b5152043470b8384edfb9d139a31fe342be
Shahriyar117 <shahriyar.rahim333@gmail.com>
2024-07-01 20:00:12 -0400

    feature: Implemented the services

--- backend/src/services/interfaces/IProductService.ts
+++ backend/src/services/interfaces/IProductService.ts
@@ -1,1 +1,10 @@
-export interface IProductService {}
+import { Product } from "../../models/product.model";
+
+export interface IProductService {
+  getAllProducts(
+    page: number,
+    pageSize: number,
+    orderByField: string,
+    sortOrder: "asc" | "desc"
+  ): Promise<{ products: Product[]; totalPages: number; totalCount: number }>;
+}
